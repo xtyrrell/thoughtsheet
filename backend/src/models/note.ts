d6a0a51edbdf1bb28956e07f27b248d42752105b
Max Tyrrell <maxtyrrell0@gmail.com>
2021-05-16 14:32:07 +0200

    feat(backend/models): add models

---
+++ backend/src/models/note.ts
@@ -0,0 +1,31 @@
+import mongoose, { Document, Schema } from 'mongoose'
+import { IUser } from './user'
+
+export interface INote extends Document {
+  userId: IUser['_id']
+  title: string,
+  body: string,
+  tags: [string]
+}
+
+const noteSchema: Schema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  body: {
+    type: String,
+    required: true,
+    default: ''
+  },
+  tags: [String]
+}, { timestamps: true })
+
+const Note = mongoose.model<INote>('Note', noteSchema)
+
+export default Note
--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,9 +1,32 @@
-import mongoose from 'mongoose'
+import mongoose, { Document, Schema } from 'mongoose'
+import validator from 'validator'
+import { INote } from './note'
 
-const userSchema = new mongoose.Schema({
-  phoneNumber: String
-})
+export interface IUser extends Document {
+  phoneNumber: string,
+  email: string,
+  password: string,
+  notes: [INote]
+}
 
-const User = mongoose.model('User', userSchema)
+const userSchema: Schema = new Schema({
+  phoneNumber: {
+    type: String,
+    unique: true,
+    validate: validator.isMobilePhone
+  },
+  email: {
+    type: String,
+    unique: true,
+    validate: validator.isEmail
+  },
+  password: String,
+  notes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Note'
+ }]
+}, { timestamps: true })
+
+const User = mongoose.model<IUser>('User', userSchema)
 
 export default User
