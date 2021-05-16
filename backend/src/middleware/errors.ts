09762a06430e7f67bcdde7ae69b818c2f5d6023b
Max Tyrrell <maxtyrrell0@gmail.com>
2021-05-16 14:34:12 +0200

    feat(backend/middleware): add error middleware

---
+++ backend/src/middleware/errors.ts
@@ -0,0 +1,28 @@
+import { Errback, NextFunction, Request, Response } from "express"
+import mongoose, { mongo } from "mongoose"
+
+export class NotFoundError extends Error {
+  status: number = 404
+
+  constructor(message='Not found') {
+    super()
+    this.message = message
+  }
+}
+
+// TODO(xtyrrell): v1: Implement proper error handling; properly account for all errors that could be
+// thrown in route handlers
+export async function errorHandler (err: Errback, req: Request, res: Response, next: NextFunction): Promise<any> {
+  console.error("Oh no, error encountered!")
+  console.error(err)
+
+  if (err instanceof NotFoundError) {
+    return res.status(err.status).send(err.message)
+  }
+
+  if (err instanceof mongoose.Error) {
+    return res.status(500).send("Mongoose error")
+  }
+
+  res.sendStatus(500)
+}
