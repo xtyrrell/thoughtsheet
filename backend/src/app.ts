97a15ab352c0b2ed5d8777bce1f44f083d897c2d
Max Tyrrell <maxtyrrell0@gmail.com>
2021-06-19 20:14:46 +0200

    fix(backend): cast middleware causing type-errors as any

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,8 +8,8 @@ const app = express();
 // app.use(cors({ credentials: true }));
 app.use(cors({ origin: /localhost:3000$/i, credentials: true }));
 
-app.use(express.json()); // for parsing application/json
-app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.json() as any); // for parsing application/json
+app.use(express.urlencoded({ extended: true }) as any); // for parsing application/x-www-form-urlencoded
 
 app.use(router);
 
