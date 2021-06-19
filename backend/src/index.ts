4a2796619fa2137f7e7dbba361b0c10ea30fc672
Max Tyrrell <maxtyrrell0@gmail.com>
2021-06-19 18:19:23 +0200

    feature(backend): add better server startup logging

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,26 +1,34 @@
-import mongoose from 'mongoose'
-import app from './app'
-import requireEnvVar from './utils/env'
+import mongoose from "mongoose";
+import app from "./app";
+import requireEnvVar from "./utils/env";
 
-async function main () {
-  const PORT = requireEnvVar('PORT')
+async function main() {
+  const PORT = requireEnvVar("PORT");
+  const MONGO_URL = requireEnvVar("MONGO_URL", "mongodb://localhost");
+
+  console.log("Starting server...");
+  console.log(
+    `Awaiting connection to a MongoDB instance at MONGO_URL: ${MONGO_URL}...`
+  );
 
   try {
-    await mongoose.connect(requireEnvVar('MONGO_URL', 'mongodb://localhost'), {
+    await mongoose.connect(MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
-      useFindAndModify: false
-    })
+      useFindAndModify: false,
+    });
   } catch {
-    console.error(`Mongoose could not connect to MongoDB server. Exiting.`)
-    process.exit(1)
+    console.error(`Mongoose could not connect to MongoDB server. Exiting.`);
+    process.exit(1);
   }
 
   // Start the express server
   app.listen(PORT, () => {
-    console.log(`Express server listening on port http://localhost:${PORT}.`)
-  })
+    console.log(
+      `Connection to MongoDB successfull. Now listening on port ${PORT}.`
+    );
+  });
 }
 
-main()
+main();
