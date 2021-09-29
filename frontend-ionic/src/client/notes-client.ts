aa7efa835185435f6c32f58f0726618fed43ae9e
Max Tyrrell <maxtyrrell0@gmail.com>
2021-09-29 09:17:37 +0200

    chore: use production URL on production

--- frontend-ionic/src/client/notes-client.ts
+++ frontend-ionic/src/client/notes-client.ts
@@ -1,29 +1,27 @@
 import axios, { AxiosResponse } from "axios";
 import { INote } from "./interfaces";
 
+const useProductionUrl = process.env.NODE_ENV === "production";
+
+const API_URL = useProductionUrl
+  ? `https://backend-api-m55qthxfka-ew.a.run.app/`
+  : `http://localhost:5000/`;
+
 export const getNoteById = async (id) => {
-  const { data } = await axios.get(
-    // `https://backend-api-m55qthxfka-ew.a.run.app/notes/${id}`
-    `http://localhost:5000/notes/${id}`
-  );
+  console.log(`getNoteById(${id}) with id`, id);
+
+  const { data } = await axios.get(API_URL + `notes/${id}`);
   return data;
 };
 
 export const updateNote = (id) => (note: Partial<INote>) => {
   console.log(`updateNote(${id}) with note`, note);
-  return axios.patch(
-    // `https://backend-api-m55qthxfka-ew.a.run.app/notes/${id}`,
-    `http://localhost:5000/notes/${id}`,
-    note
-  );
+
+  return axios.patch(API_URL + `notes/${id}`, note);
 };
 
 export const createNote = (note: Partial<INote>) => {
   console.log(`createNote(note) where note=`, note);
 
-  return axios.post(
-    // `https://backend-api-m55qthxfka-ew.a.run.app/notes/${id}`,
-    `http://localhost:5000/notes`,
-    note
-  ) as Promise<AxiosResponse<INote>>;
+  return axios.post(API_URL + `notes/`, note) as Promise<AxiosResponse<INote>>;
 };
